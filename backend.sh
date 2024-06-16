1be3e1868c1df1d938f6537250697bb61d81ebac
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 20:20:35 -0400

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -118,8 +118,8 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-# mysql -h db.mohansaivenna.cloud -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-# VALIDATE $? "Schema loading"
+mysql -h db.mohansaivenna.cloud -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
 
-# systemctl restart backend &>>$LOGFILE
-# VALIDATE $? "Restarting Backend"
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
