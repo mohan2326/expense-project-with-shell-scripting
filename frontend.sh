9d23e4837d1aae490c12159883930d5169a4a543
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 23:56:21 -0400

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -47,7 +47,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user//home/ec2-user/expense-project-with-shell-scripting/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/expense-project-with-shell-scripting/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
