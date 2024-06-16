3801d0e3a03c39abec900ba95af6a80e749c732d
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 20:40:38 -0400

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -122,7 +122,7 @@ VALIDATE $? "Installing MySQL Client"
 systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "Enabling MySQL Client"
 
-dnf systemctl start mysqld -y &>>$LOGFILE
+systemctl start mysqld -y &>>$LOGFILE
 VALIDATE $? "Starting MySQL Client"
 
 mysql -h db.mohansaivenna.cloud -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
